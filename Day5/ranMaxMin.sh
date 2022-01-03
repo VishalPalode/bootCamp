37e5a52513e7855711f9b8edefed0308b8304d30
VishalPalode <96374249+VishalPalode@users.noreply.github.com>
2022-01-03 10:11:42 +0530

    Update ranMaxMin.sh

--- Day5/ranMaxMin.sh
+++ Day5/ranMaxMin.sh
@@ -1,1 +1,19 @@
+  GNU nano 5.9                                                                                              ranMaxMin.sh
+#!/bin/bash -x
 
+minvalue=1000
+maxvalue=0
+for(( i=0; i<5; i++ ))
+do
+randnum=$((RANDOM%900+100))
+echo $randnum
+if [ $randnum -gt $maxvalue ]
+then
+maxvalue=$randnum
+elif [ $randnum -lt $minvalue ]
+then
+minvalue=$randnum
+fi
+done
+echo "maxvalue= " $maxvalue
+echo "minvalue= " $minvalue
